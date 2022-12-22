2c85c6b17cabd00ec402a1a2a5e226454b233c7c
anjali790 <anjaliruby790@gmail.com>
2022-12-22 19:09:24 +0530

    practice

---
+++ Currying/app.js
@@ -0,0 +1,15 @@
+function x(p) {
+    return function (q) {
+        return function (r) {
+            return p * q * r;
+        }
+    }
+}
+console.log(x(5)(6)(7));
+
+
+
+function calculate(l, b, h) {
+    return l * b * h;
+}
+console.log(calculate(2, 3, 4));
